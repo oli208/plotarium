e10a98f179a343f0659327d9886be4a2bea166c1
oli208 <68897677+oli208@users.noreply.github.com>
2025-11-13 20:01:27 +0100

    added default option to run App in background

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,28 @@
-#' Launch Plotarium Addin
+#' Launch Plotarium App
 #' @export
-run_plotarium <- function() {
-  app_dir <- system.file("app", package = "plotarium")
-  if (app_dir == "" || app_dir == ".") {
-    stop("Plotarium app not found in installed package. Please run from package source or install the package.")
-  }
-  shiny::runApp(app_dir, launch.browser = TRUE)
+# run_plotarium <- function() {
+#   app_dir <- system.file("app", package = "plotarium")
+#   if (app_dir == "" || app_dir == ".") {
+#     stop("Plotarium app not found in installed package. Please run from package source or install the package.")
+#   }
+#   shiny::runApp(app_dir, launch.browser = TRUE)
+# }
+
+
+run_plotarium <- function(launch.browser = TRUE, background = TRUE) {
+    app_dir <- system.file("app", package = "plotarium")
+    if (app_dir == "" || app_dir == ".") {
+        stop("Plotarium app not found in installed package. Please run from package source or install the package.")
+    }
+
+    if (background) {
+        processx::process$new(
+            "R",
+            c("-e", sprintf("shiny::runApp('%s', launch.browser = %s)", app_dir, launch.browser)),
+            stdout = "|", stderr = "|"
+        )
+        message("🚀 Plotarium launched in the background.")
+    } else {
+        shiny::runApp(app_dir, launch.browser = launch.browser)
+    }
 }
